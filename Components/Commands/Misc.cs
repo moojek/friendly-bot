e9a74a3b1fad1930d4bfb61bbdd3463a3ab24302
moojek <lemonid2002@gmail.com>
2018-12-06 23:14:53 +0100

    Added mostroles command

--- Components/Commands/Misc.cs
+++ Components/Commands/Misc.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using System;
+using Discord.WebSocket;
+using System.Collections.Generic;
 
 namespace FriendlyBot.Components.Commands
 {
@@ -25,6 +28,29 @@ namespace FriendlyBot.Components.Commands
             {
                 if (!user.IsBot)
                     await user.SendMessageAsync(message);
+            }
+        }
+
+        [Command("mostroles")]
+        public async Task GetUserWithMostRoles()
+        {
+            var users = Context.Guild.Users;
+            // Tuple<SocketGuildUser,int> max = new Tuple<SocketGuildUser, int>(null, 0);
+            int max = 0;
+            List<SocketGuildUser> max_users = new List<SocketGuildUser>();
+            foreach (var user in users)
+            {
+                var roles = user.Roles;
+                if (roles.Count > max)
+                {
+                    max = roles.Count;
+                    max_users.Clear();
+                    max_users.Add(user);
+                }
+            }
+            foreach (var user in max_users)
+            {
+                await Context.Channel.SendMessageAsync(user.Nickname);
             }
         }
     }
